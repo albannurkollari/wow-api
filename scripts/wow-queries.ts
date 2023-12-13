0bfec22a36e8baf0663e19433024de03b3e575f2
Alban Nurkollari <albannurkollari@gmail.com>
2023-12-13 15:21:34 +0100

    feat: adds realm, item, auctions and commodities queries

--- scripts/wow-queries.ts
+++ scripts/wow-queries.ts
@@ -1,5 +1,10 @@
-import { writeFile } from "node:fs/promises";
+import { mkdir, writeFile } from "node:fs/promises";
 import * as prettier from "prettier";
+import dotenv from "dotenv";
+
+import { CONNECTED_REALMS, CONNECTED_REALM_IDS } from "../src/constants/servers";
+
+import { kebabCase } from "change-case";
 
 // Constants
 import {
@@ -9,16 +14,27 @@ import {
   NAMESPACES,
   LOCALE,
   LOCALES,
-} from "#constants/api";
+} from "../src/constants/api";
+import path from "node:path";
 
-debugger;
+dotenv.config({ path: ".env.local" });
+
 type Namespace = "dynamic" | "static" | "profile";
 type Region = "eu" | "us";
-type WoWQueryType = "auctions" | "item";
+type WoWQueryType = "auctions" | "commodities" | "item" | "realms";
+type WoWQueryTypeFlag = "-a" | "-c" | "-i" | "-r";
 type WoWURLOptions = {
   crID?: number;
   itemID?: number;
   region?: Region;
+};
+type RealmsWithId = { buyout: number; realms?: string[] };
+type ItemInfo = {
+  id: number | string;
+  isCommodity?: boolean;
+  name: string;
+  description: string;
+  prices: { cheapest?: RealmsWithId | number; servers?: Record<string, RealmsWithId> };
 };
 
 const getCommonParams = (accessToken: string, namespace: Namespace, region: Region) =>
@@ -31,7 +47,7 @@ const getCommonParams = (accessToken: string, namespace: Namespace, region: Regi
 const craftWoWURLWithToken = (
   type: WoWQueryType,
   accessToken = process.env.CURRENT_ACCESS_TOKEN,
-  { crID, itemID, region }: WoWURLOptions = { region: "eu", crID: 3674 },
+  { crID, itemID = LOCAL_ITEM_ID, region = "eu" }: WoWURLOptions = {},
 ) => {
   const _url = new URL(`https://${region}.api.blizzard.com/`);
   const basePath = "/data/wow";
@@ -42,9 +58,19 @@ const craftWoWURLWithToken = (
       _url.search = getCommonParams(accessToken, NAMESPACES.DYNAMIC, region);
       break;
     }
+    case "commodities": {
+      _url.pathname = `${basePath}/auctions/commodities`;
+      _url.search = getCommonParams(accessToken, NAMESPACES.DYNAMIC, region);
+      break;
+    }
     case "item": {
       _url.pathname = `${basePath}/item/${itemID}`;
       _url.search = getCommonParams(accessToken, NAMESPACES.STATIC, region);
+      break;
+    }
+    case "realms": {
+      _url.pathname = `${basePath}/connected-realm/${crID}`;
+      _url.search = getCommonParams(accessToken, NAMESPACES.DYNAMIC, region);
       break;
     }
   }
@@ -52,79 +78,177 @@ const craftWoWURLWithToken = (
   return _url.toString();
 };
 
-// Pattern: Shadowflame-Tempered Armor Patch
-const PATTERN_ITEM_ID = 204968;
+let LOCAL_ITEM_ID = 0;
+let IS_COMMODITY = false;
+let QUERY_TYPE: WoWQueryTypeFlag = "-a";
+let DEBUG_LEVEL = 0;
 
-const grabCheapestItemFromAuctions = ({ auctions = [] } = {}, itemId = PATTERN_ITEM_ID) => {
-  let items = [];
-  let theItem = {};
+const grabCheapestItemFromAuctions = ({ auctions = [] } = {}, itemID = LOCAL_ITEM_ID) => {
+  let smallestBuyoutItem = null;
+  let smallestBuyout = Number.POSITIVE_INFINITY;
 
-  for (let i = 0; i < auctions.length; i++) {
-    const ahItem = auctions[i];
+  for (const auction of auctions) {
+    if (auction.item.id === itemID) {
+      const priceProp = IS_COMMODITY ? "unit_price" : "buyout";
 
-    if (ahItem.item.id === itemId) {
-      items.push(ahItem);
-
-      if (items.length > 1) {
-        theItem = items.at(-2).buyout > ahItem.buyout ? ahItem : items.at(-2);
+      if (auction[priceProp] < smallestBuyout) {
+        smallestBuyout = auction.buyout;
+        smallestBuyoutItem = auction;
       }
     }
   }
 
-  if (items.length === 1) {
-    theItem = items[0];
+  if (smallestBuyoutItem?.hasOwnProperty("unit_price")) {
+    smallestBuyoutItem.buyout = smallestBuyoutItem.unit_price;
+    delete smallestBuyoutItem.unit_price;
   }
 
-  return theItem;
+  return smallestBuyoutItem as { buyout: number };
 };
 
-const writeToFile = (jsonData = {}, filename) =>
-  prettier
-    .format(JSON.stringify(jsonData), { parser: "json" })
-    .then((data) => writeFile(`wow_${filename || "random"}`, data, { encoding: "utf-8" }))
-    .then(() => console.log("Success ✅"));
+const writeToFile = (() => {
+  async function handleWriteFile(filePath: string, content: string) {
+    try {
+      const directoryPath = path.dirname(filePath);
 
-const getAllAuctions = () =>
-  fetch(craftWoWURLWithToken("auctions"), DEFAULT_HEADERS)
+      await mkdir(directoryPath, { recursive: true });
+      await writeFile(filePath, content, { encoding: "utf-8" });
+
+      console.log(`✅ File created successfully!\n ${filePath}`);
+    } catch (err) {
+      console.error("❌ Error writing to file:", err);
+    }
+  }
+
+  return (jsonData = {}, fileName: string) =>
+    prettier
+      .resolveConfig("./.prettierrc.json")
+      .then((options) => prettier.format(JSON.stringify(jsonData), { ...options, parser: "json" }))
+      .then((data) => handleWriteFile(fileName, data));
+})();
+
+const getAllAuctions = (...args: any[]) => {
+  const method = IS_COMMODITY ? "commodities" : "auctions";
+  const url = craftWoWURLWithToken(method, ...args);
+  DEBUG_LEVEL > 0 && console.log(url);
+
+  return fetch(url, DEFAULT_HEADERS)
     .then((response) => response.json())
     .catch(console.error);
+};
 
-const getShadowflameTemperedPatch = () =>
-  getAllAuctions()
-    .then(grabCheapestItemFromAuctions)
-    .then(async ({ buyout } = { buyout: 10000 }) => {
-      /* {
-        "id": 116630933,
-        "item": { "id": 204968, "context": 3 },
-        "bid": 144007000,
-        "buyout": 180008800,
-        "quantity": 1,
-        "time_left": "LONG"
-      } */
+const grabItemInfo = async (rawInfo = false): Promise<ItemInfo> => {
+  try {
+    const url = craftWoWURLWithToken("item", process.env.CURRENT_ACCESS_TOKEN);
+    const result = await (await fetch(url, DEFAULT_HEADERS)).json();
+    const { name, description } = result;
 
-      const url = craftWoWURLWithToken("item", process.env.CURRENT_ACCESS_TOKEN, {
-        itemID: PATTERN_ITEM_ID,
-        crID: 3674,
-      });
+    if (rawInfo) return result;
 
-      try {
-        const itemInfo = await (await fetch(url, DEFAULT_HEADERS)).json();
-        debugger;
+    return {
+      id: LOCAL_ITEM_ID,
+      name,
+      description,
+      prices: { ...(!IS_COMMODITY && { servers: {} }) },
+    };
+  } catch (error) {}
+};
 
-        const requiredItemInfo = {
-          name: itemInfo.name,
-          description: itemInfo.description,
-          price: buyout / 10000,
-        };
+const startQueries = async () => {
+  switch (QUERY_TYPE) {
+    case "-a": {
+      const item = await grabItemInfo();
+      console.log(`--> Fetching prices across all EU servers for item:\n ${item.name}!\n`);
 
-        return requiredItemInfo;
+      await Promise.all(
+        CONNECTED_REALMS.map(({ id: crID, realms }) =>
+          getAllAuctions(process.env.CURRENT_ACCESS_TOKEN, { crID }).then((data) => {
+            try {
+              const auction = grabCheapestItemFromAuctions(data);
 
-        // writeToFile(
-        //   JSON.stringify(requiredItemInfo, null, 2),
-        //   "pattern_shadowflame_tempered_patch",
-        // );
-      } catch (error) {
-        debugger;
-        console.error(error);
+              if (!!auction?.buyout) {
+                item.prices.servers[crID] = {
+                  buyout: auction.buyout / 10000,
+                  realms: [...realms],
+                };
+              }
+            } catch (error) {
+              console.error("Error grabbing cheapest item from auctions!");
+              console.error(error);
+            }
+          }),
+        ),
+      );
+
+      const cheapest = Object.values(item.prices.servers).sort((a, b) => a.buyout - b.buyout)?.[0];
+      item.prices.cheapest = cheapest;
+
+      writeToFile(item, `./data/auctions/${LOCAL_ITEM_ID}-${kebabCase(item.name)}.json`);
+      break;
+    }
+    case "-c": {
+      const item = await grabItemInfo();
+      console.log(`--> Fetching prices across all EU servers for item:\n ${item.name}!\n`);
+
+      const data = await getAllAuctions(process.env.CURRENT_ACCESS_TOKEN);
+      const auction = grabCheapestItemFromAuctions(data);
+
+      if (!!auction?.buyout) {
+        item.prices.cheapest = auction.buyout / 10000;
       }
-    });
+
+      writeToFile(item, `./data/commodities/${LOCAL_ITEM_ID}-${kebabCase(item.name)}.json`);
+      break;
+    }
+    case "-i":
+      const item = await grabItemInfo(true);
+
+      writeToFile(item, `./data/items/${LOCAL_ITEM_ID}-${kebabCase(item.name)}.json`);
+      break;
+    case "-r": {
+      try {
+        const realmsResponse = await Promise.allSettled<{ id: number; realms: string[] }>(
+          CONNECTED_REALM_IDS.map((crID) => {
+            const url = craftWoWURLWithToken("realms", process.env.CURRENT_ACCESS_TOKEN, { crID });
+
+            return fetch(url, DEFAULT_HEADERS)
+              .then((res) => res.json())
+              .then((res) => ({ id: res.id, realms: res.realms.map(({ slug }) => slug) }));
+          }),
+        );
+        const result = realmsResponse as {
+          status: "fulfilled" | "rejected";
+          value: { id: number; realms: string[] };
+        }[];
+
+        const realmsData = result
+          .filter((item) => item.status === "fulfilled")
+          .map((item) => item.value);
+
+        writeToFile(realmsData, "./data/realms.json");
+      } catch (error) {
+        console.log(error);
+      }
+
+      break;
+    }
+    default:
+      throw new Error("Invalid or unsupported WoW API query type!");
+  }
+};
+
+if (process.argv.length === 3) {
+  LOCAL_ITEM_ID = parseInt(process.argv.at(-1));
+} else if (process.argv.length === 4) {
+  LOCAL_ITEM_ID = parseInt(process.argv.at(-2));
+  QUERY_TYPE = process.argv.at(-1) as WoWQueryTypeFlag;
+  IS_COMMODITY = QUERY_TYPE === "-c";
+  DEBUG_LEVEL = !IS_COMMODITY ? parseInt(process.argv.at(-1)) : 0;
+} else if (process.argv.length === 5) {
+  LOCAL_ITEM_ID = parseInt(process.argv.at(-3));
+  QUERY_TYPE = process.argv.at(-2) as WoWQueryTypeFlag;
+  IS_COMMODITY = QUERY_TYPE === "-c";
+  DEBUG_LEVEL = parseInt(process.argv.at(-1)) || 0;
+}
+
+startQueries();
---
+++ src/constants/servers.ts
@@ -0,0 +1,435 @@
+export const CONNECTED_REALM_IDS = [
+  1080, 1084, 1596, 1597, 1598, 1602, 1604, 1605, 1614, 604, 1091, 1092, 1097, 1098, 1104, 1105,
+  1121, 1122, 1127, 1402, 1416, 531, 510, 512, 612, 633, 1331, 1401, 1403, 1406, 1621, 1624, 578,
+  580, 1615, 1307, 1309, 581, 509, 1325, 1329, 1335, 1313, 1316, 1623, 570, 1096, 1082, 1378, 1379,
+  1384, 1385, 1388, 1393, 1396, 1400, 1099, 1618, 1390, 1925, 1928, 1929, 1405, 1417, 2073, 2074,
+  1302, 1303, 1305, 1085, 1315, 1587, 1301, 1922, 1923, 1408, 3391, 3656, 3657, 3666, 3702, 3703,
+  3713, 3674, 3679, 3681, 3682, 3686, 3690, 3691, 3692, 3696,
+] as const;
+
+export const CONNECTED_REALMS = [
+  { id: 1080, realms: ["khadgar", "bloodhoof"] },
+  {
+    id: 1084,
+    realms: ["dentarg", "tarren-mill"],
+  },
+  {
+    id: 1596,
+    realms: [
+      "karazhan",
+      "dragonblight",
+      "the-maelstrom",
+      "ghostlands",
+      "lightnings-blade",
+      "deathwing",
+    ],
+  },
+  {
+    id: 1597,
+    realms: ["auchindoun", "dunemaul", "sylvanas", "jaedenar"],
+  },
+  {
+    id: 1598,
+    realms: [
+      "shattered-halls",
+      "chromaggus",
+      "sunstrider",
+      "balnazzar",
+      "talnivarr",
+      "ahnqiraj",
+      "daggerspine",
+      "laughing-skull",
+      "trollbane",
+      "boulderfist",
+    ],
+  },
+  {
+    id: 1602,
+    realms: ["gordunni"],
+  },
+  {
+    id: 1604,
+    realms: ["soulflayer"],
+  },
+  {
+    id: 1605,
+    realms: ["deathguard"],
+  },
+  {
+    id: 1614,
+    realms: ["galakrond", "deepholm", "razuvious"],
+  },
+  {
+    id: 604,
+    realms: ["thrall", "kargath", "ambossar"],
+  },
+  {
+    id: 1091,
+    realms: ["emeriss", "twilights-hammer", "bloodscalp", "crushridge", "agamaggan", "hakkar"],
+  },
+  {
+    id: 1092,
+    realms: ["drakthul", "burning-blade"],
+  },
+  {
+    id: 1097,
+    realms: ["ysera", "malorne"],
+  },
+  {
+    id: 1098,
+    realms: ["malygos", "malfurion"],
+  },
+  {
+    id: 1104,
+    realms: ["anetheron", "kiljaeden", "rajaxx", "festung-der-stürme", "guldan", "nathrezim"],
+  },
+  {
+    id: 1105,
+    realms: ["nazjatar", "zuluhed", "dalvengyr", "amanthul", "frostmourne", "anubarak"],
+  },
+  {
+    id: 1121,
+    realms: [
+      "die-arguswacht",
+      "die-ewige-wacht",
+      "die-todeskrallen",
+      "das-syndikat",
+      "der-abyssische-rat",
+      "kult-der-verdammten",
+      "das-konsortium",
+      "die-silberne-hand",
+    ],
+  },
+  {
+    id: 1122,
+    realms: ["uldaman", "drekthar", "krasus", "eitrigg"],
+  },
+  {
+    id: 1127,
+    realms: [
+      "confrérie-du-thorium",
+      "la-croisade-écarlate",
+      "culte-de-la-rive-noire",
+      "les-sentinelles",
+      "kirin-tor",
+      "les-clairvoyants",
+      "conseil-des-ombres",
+    ],
+  },
+  {
+    id: 1402,
+    realms: ["turalyon", "doomhammer"],
+  },
+  {
+    id: 1416,
+    realms: ["blades-edge", "eonar", "veknilash", "aerie-peak", "bronzebeard"],
+  },
+  {
+    id: 531,
+    realms: ["dethecus", "theradras", "onyxia", "mugthol", "terrordar"],
+  },
+  {
+    id: 510,
+    realms: ["voljin", "chants-éternels"],
+  },
+  {
+    id: 512,
+    realms: ["arakarahm", "rashgarroth", "kaelthas", "throkferoth"],
+  },
+  {
+    id: 612,
+    realms: ["nefarian", "gilneas", "destromath", "ulduar", "mannoroth", "gorgonnash", "nerathor"],
+  },
+  {
+    id: 633,
+    realms: [
+      "korgall",
+      "executus",
+      "shattered-hand",
+      "bloodfeather",
+      "terokkar",
+      "saurfang",
+      "darkspear",
+      "burning-steppes",
+    ],
+  },
+  {
+    id: 1331,
+    realms: ["suramar", "medivh"],
+  },
+  {
+    id: 1401,
+    realms: ["garrosh", "shattrath", "nozdormu", "perenolde", "teldrassil"],
+  },
+  {
+    id: 1403,
+    realms: ["draenor"],
+  },
+  {
+    id: 1406,
+    realms: ["arygos", "khazgoroth"],
+  },
+  {
+    id: 1621,
+    realms: ["marécage-de-zangar", "chogall", "eldrethalas", "sinstralis", "dalaran"],
+  },
+  {
+    id: 1624,
+    realms: ["naxxramas", "arathi", "illidan", "temple-noir"],
+  },
+  {
+    id: 578,
+    realms: ["arthas", "blutkessel", "wrathbringer", "durotan", "kelthuzad", "veklor", "tirion"],
+  },
+  {
+    id: 580,
+    realms: ["blackmoore", "tichondrius", "lordaeron"],
+  },
+  {
+    id: 1615,
+    realms: ["howling-fjord"],
+  },
+  {
+    id: 1307,
+    realms: ["chamber-of-aspects"],
+  },
+  {
+    id: 1309,
+    realms: ["pozzo-delleternità"],
+  },
+  {
+    id: 581,
+    realms: ["blackrock"],
+  },
+  {
+    id: 509,
+    realms: ["garona", "sargeras", "nerzhul"],
+  },
+  {
+    id: 1325,
+    realms: ["aggramar", "hellscream"],
+  },
+  {
+    id: 1329,
+    realms: ["ravencrest"],
+  },
+  {
+    id: 1335,
+    realms: ["ysondre"],
+  },
+  {
+    id: 1313,
+    realms: ["wildhammer", "thunderhorn"],
+  },
+  {
+    id: 1316,
+    realms: ["nemesis"],
+  },
+  {
+    id: 1623,
+    realms: ["fordragon"],
+  },
+  {
+    id: 570,
+    realms: ["lothar", "baelgun", "azshara", "kragjin"],
+  },
+  {
+    id: 1096,
+    realms: [
+      "scarshield-legion",
+      "sporeggar",
+      "earthen-ring",
+      "defias-brotherhood",
+      "ravenholdt",
+      "darkmoon-faire",
+      "the-venture-co",
+    ],
+  },
+  {
+    id: 1082,
+    realms: ["kul-tiras", "alonsus", "anachronos"],
+  },
+  {
+    id: 1378,
+    realms: ["dun-modr", "cthun"],
+  },
+  {
+    id: 1379,
+    realms: ["zuljin", "uldum", "sanguino", "shendralar"],
+  },
+  {
+    id: 1384,
+    realms: ["tyrande", "los-errantes", "colinas-pardas"],
+  },
+  {
+    id: 1385,
+    realms: ["exodar", "minahonda"],
+  },
+  {
+    id: 1388,
+    realms: ["lightbringer", "mazrigos"],
+  },
+  {
+    id: 1393,
+    realms: ["bronze-dragonflight", "nordrassil"],
+  },
+  {
+    id: 1396,
+    realms: ["quelthalas", "azjolnerub"],
+  },
+  {
+    id: 1400,
+    realms: ["ungoro", "senjin", "area-52"],
+  },
+  {
+    id: 1099,
+    realms: ["rexxar", "alleria"],
+  },
+  {
+    id: 1618,
+    realms: ["die-aldor"],
+  },
+  {
+    id: 1390,
+    realms: ["hyjal"],
+  },
+  {
+    id: 1925,
+    realms: ["eversong"],
+  },
+  {
+    id: 1928,
+    realms: ["goldrinn", "lich-king", "greymane"],
+  },
+  {
+    id: 1929,
+    realms: ["blackscar", "deathweaver", "borean-tundra", "thermaplugg", "grom", "booty-bay"],
+  },
+  {
+    id: 1405,
+    realms: [
+      "todeswache",
+      "zirkel-des-cenarius",
+      "der-mithrilorden",
+      "forscherliga",
+      "der-rat-von-dalaran",
+      "die-nachtwache",
+    ],
+  },
+  {
+    id: 1417,
+    realms: ["azuremyst", "stormrage"],
+  },
+  {
+    id: 2073,
+    realms: ["stormscale"],
+  },
+  {
+    id: 2074,
+    realms: ["terenas", "emerald-dream"],
+  },
+  {
+    id: 1302,
+    realms: ["archimonde"],
+  },
+  {
+    id: 1303,
+    realms: ["grim-batol", "aggra-português", "frostmane"],
+  },
+  {
+    id: 1305,
+    realms: ["kazzak"],
+  },
+  {
+    id: 1085,
+    realms: ["moonglade", "steamwheedle-cartel", "the-shatar"],
+  },
+  {
+    id: 1315,
+    realms: ["elune", "varimathras"],
+  },
+  {
+    id: 1587,
+    realms: ["hellfire", "runetotem", "arathor", "kilrogg", "nagrand"],
+  },
+  {
+    id: 1301,
+    realms: ["outland"],
+  },
+  {
+    id: 1922,
+    realms: ["azuregos"],
+  },
+  {
+    id: 1923,
+    realms: ["ashenvale"],
+  },
+  {
+    id: 1408,
+    realms: ["norgannon", "dun-morogh"],
+  },
+  {
+    id: 3391,
+    realms: ["silvermoon"],
+  },
+  {
+    id: 3656,
+    realms: ["spinebreaker", "dragonmaw", "vashj", "stormreaver", "haomarush"],
+  },
+  {
+    id: 3657,
+    realms: ["genjuros", "zenedar", "bladefist", "neptulon", "frostwhisper", "darksorrow"],
+  },
+  {
+    id: 3666,
+    realms: ["shadowsong", "aszune"],
+  },
+  {
+    id: 3702,
+    realms: ["argent-dawn"],
+  },
+  {
+    id: 3703,
+    realms: ["frostwolf"],
+  },
+  {
+    id: 3713,
+    realms: ["alakir", "skullcrusher", "xavius", "burning-legion"],
+  },
+  {
+    id: 3674,
+    realms: ["twisting-nether"],
+  },
+  {
+    id: 3679,
+    realms: ["aegwynn"],
+  },
+  {
+    id: 3681,
+    realms: ["magtheridon"],
+  },
+  {
+    id: 3682,
+    realms: ["ragnaros"],
+  },
+  {
+    id: 3686,
+    realms: ["antonidas"],
+  },
+  {
+    id: 3690,
+    realms: ["khaz-modan"],
+  },
+  {
+    id: 3691,
+    realms: ["echsenkessel", "blackhand", "malganis", "taerar"],
+  },
+  {
+    id: 3692,
+    realms: ["eredar"],
+  },
+  {
+    id: 3696,
+    realms: ["nethersturm", "alexstrasza", "madmortem", "proudmoore"],
+  },
+] as const;
